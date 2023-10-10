5d45626a6f3d3668a76b9c98af80ac4c4b062e97
Claudio Rivera Bravo <riverabravocl@outlook.com>
2023-10-10 20:18:19 -0300

    probando desde el navegador

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,48 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    </head>
-    <body>
-        <h4>Listado de Etiquetas</h4>
-        @forelse ($tags as $tag)
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <form action="tags" method="post">
+        @csrf
+        <input type="text" name="name">
+        <input type="submit" value="Agregar">
+
+
+    </form>
+    <h4>Listado de Etiquetas</h4>
+    @forelse ($tags as $tag)
         <tr>
             <td>
-                {{$tag->id}}
+                {{ $tag->name }}
             </td>
-            <td>
-                {{$tag->name}}<br>
+            <form action="tags/{{ $tag->id }}" method="post">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Eliminar">
+
+            </form>
             </td>
         </tr>
-            
-        @empty
+
+    @empty
         <tr>
             <td>
                 <p>No hay etiquetas</p>
             </td>
         </tr>
-            
-        @endforelse
-   
-    </body>
+    @endforelse
+
+</body>
+
 </html>
