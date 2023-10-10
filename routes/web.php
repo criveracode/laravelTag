ea21f00823112715d3588a573771cc384c2461fa
Claudio Rivera Bravo <riverabravocl@outlook.com>
2023-10-10 19:49:33 -0300

    registro nuevo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TagController;
 use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,4 @@ Route::get('/', function () {
     ]);
 });
 
+Route::post('tags',[TagController::class,'store']);
